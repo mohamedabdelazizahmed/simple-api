2a1d87d4a914edc5c5adf31ce96f95cf27f7f002
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-15 18:24:19 +0200

    3-REST APIs, Clients & CORS Errors-Post Request

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 /**
- *  npm init , npm install --save express body-parser 
- *  npm install --save-dev nodemon 
+ *  npm init , npm install --save express body-parser
+ *  npm install --save-dev nodemon
  */
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 const app = express();
 
 // app.use(bodyParser.urlencoded())  DataFormat in form  x-www-form-urlencoded <form>
 app.use(bodyParser.json()); //in request header application/json
 
-app.use('/feed', feedRoutes);
+/** Resolve the CROS problem  */
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.use("/feed", feedRoutes);
 
 app.listen(8080);
