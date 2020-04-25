a0e29ab3e17e1eb5f87453db17695bd14c05a818
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-25 23:29:35 +0200

    Storing Posts in the Database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,7 @@
 const { validationResult } = require("express-validator");
+const Post = require('../models/post');
+
+
 exports.getPosts = (req, res, next) => {
   // Sending the json response
   // you can see response header [Content-Type:application/json]
@@ -32,17 +35,20 @@ exports.createPost = (req, res, next) => {
   title = req.body.title;
   content = req.body.content;
   // create post in DB.
-
-  res.status(201).json({
-    message: "The post created Successfully ",
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "Mohamedabdelaziz",
-      },
-      createdAt: new Date(),
-    },
-  });
+   const post = new Post ({
+    title: title,
+    imageUrl:'images/part-one/CROS.PNG',
+    content: content,
+    creator: {
+      name: "Mohamedabdelaziz",
+    }
+   });
+   post.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: "The post created Successfully ",
+      post: result
+    });
+   }).catch(err=> console.log(err));
+  
 };
