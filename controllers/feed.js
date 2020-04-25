bffc196c6fc4dbe0b68904299b31b6b5dcbbf043
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-04-25 17:29:37 +0200

    Fetching-list-posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,22 +1,33 @@
 exports.getPosts = (req, res, next) => {
-  // Sending the json response  
-  // you can see response header [Content-Type:application/json] 
+  // Sending the json response
+  // you can see response header [Content-Type:application/json]
   res.status(200).json({
-    posts: [{ title: "First Post", content: "the first paragraph for post" }],
+    posts: [
+      {
+        _id: '1',
+        title: "First Post",
+        content: "the first paragraph for post",
+        imageUrl: "images/part-1/CROS.PNG",
+        creator:{
+          name: 'Mohamedabdelaziz'
+        },
+        createdAt: new Date()
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
-    title = req.body.title ;
-    content = req.body.content;
-    // create post in DB.
+  title = req.body.title;
+  content = req.body.content;
+  // create post in DB.
 
   res.status(201).json({
     message: "The post created Successfully ",
     post: {
-        id: new Date().toISOString(),
-        title: title,
-        content: content
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
     },
   });
 };
