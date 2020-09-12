5e5912d5de1bd352673f84b5f220100a342d06b0
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-09-12 22:24:53 +0200

    21-Starting with User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,54 @@
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.signup = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const error = new Error('Validation failed');
-        error.statusCode = 422;
-        error.data = errors.array();
-        throw error;
-    }
-    const email = req.body.email;
-    const password = req.body.password;
-    
+  // add validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  // getting data
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  bcrypt
+    .hash(password, 10)
+    .then((hashedPw) => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashedPw,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: " User Created !", userId: result._id });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
 };
