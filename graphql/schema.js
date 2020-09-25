ae0b725fbb8a574e61b7bba77feb3a4fc3bacb17
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-09-25 16:08:48 +0200

    4. Defining a Mutation Schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,14 +1,41 @@
-const { buildSchema } = require('graphql'); 
+const { buildSchema } = require("graphql");
 module.exports = buildSchema(`
-    type TestData{
-        text: String!
-        views: Int!
-    }
-    type RootQuery{
-        hello: TestData!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
     }
 
-    schema{
-        query: RootQuery
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
     }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootQuery {
+        hello: String
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+
 `);
